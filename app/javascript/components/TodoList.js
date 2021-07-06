335e3adf7cc12369fe2f4031fe79542a8f286d11
Keito Nakamori <akiresu133@gmail.com>
2021-07-07 02:20:21 +0900

    deploy settings

--- app/javascript/components/TodoList.js
+++ app/javascript/components/TodoList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'reacyt-router-dom'
 import axios from 'axios'
 import styled from 'styled-components'
 import { ImCheckboxChecked, ImCheckboxUnchecked } from 'react-icons/im'
